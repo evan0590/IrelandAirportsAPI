46517a509eb97ac93b535dec384ba9b965913f22
Evan Byrnes <evan.byrnes@ucdconnect.ie>
2021-04-30 20:03:05 +0100

    amend new test 11

--- api/test/api/testarrivals.cpp
+++ api/test/api/testarrivals.cpp
@@ -75,7 +75,7 @@ void TestArrivals::getDate()
     const char test_flight_date[11] = "2021-02-20";
     std::string test_flight_date_s = "2021-02-20";
     QJsonArray arrivals = Arrivals::getDate(test_flight_date);
-    QVERIFY(arrivals[0].toString().contains(test_flight_date_s));
+    QVERIFY(arrivals[0].toString().contains(test_flight_date_s);
     QVERIFY(arrivals.size() > 0);
     // Arrivals arrival = Arrivals(arrivals[0]);
     // QCOMPARE(arrival.flightDate(), flight_date);
